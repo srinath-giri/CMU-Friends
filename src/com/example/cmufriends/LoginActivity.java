5e0e5c87f05107c789943537d7d36743276037e3
Vishal Jeet <vjeet10@gmail.com>
2014-04-29 16:16:52 -0400

    test

--- src/com/example/cmufriends/LoginActivity.java
+++ src/com/example/cmufriends/LoginActivity.java
@@ -10,7 +10,7 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
+		int i=0;
 	}
 
 	@Override
