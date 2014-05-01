136f9e4c9f6520493fdd30e648b2bbd9c0e03209
Srinath Giri <srinathcbe43@gmail.com>
2014-04-30 20:50:25 -0400

    Profile Activity

--- gen/com/example/cmufriends/R.java
+++ gen/com/example/cmufriends/R.java
@@ -83,11 +83,14 @@ public final class R {
         public static final int login=0x7f070001;
     }
     public static final class string {
-        public static final int IntroString=0x7f050003;
-        public static final int action_settings=0x7f050001;
+        public static final int IntroString=0x7f050004;
+        public static final int action_settings=0x7f050002;
+        public static final int andrew_domain=0x7f050007;
+        public static final int app_id=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_home=0x7f050004;
+        public static final int hello_world=0x7f050003;
+        public static final int title_activity_home=0x7f050005;
+        public static final int title_activity_profile=0x7f050006;
     }
     public static final class style {
         /** 
--- src/com/example/cmufriends/ProfileActivity.java
+++ src/com/example/cmufriends/ProfileActivity.java
@@ -1,17 +1,26 @@
 package com.example.cmufriends;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 public class ProfileActivity extends Activity implements OnClickListener {
 
@@ -23,35 +32,43 @@ public class ProfileActivity extends Activity implements OnClickListener {
 	private TextView textViewProfileFacebookID;
 	private TextView textViewProfileEmail;
 	
-	private String andrewID = "yuzhang1";
-	private String gender = "male";
-	private String facebookID = "cheng.lap.9";
-	private String email = "yuzhang@andrew.cmu.edu";
-	private String latitude = "49.00";
-	private String longditude = "49.00";
+	private String andrewID = "";
+	private String name = "";
+	private String facebookID = "";
+	private String facebookNumberID = "";
+	private String gender = "";
+	private double latitude;
+	private double longitude;
+
+	private SocialTask socialTask;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_register);
-
+		setContentView(R.layout.activity_profile);
 		textViewProfileName = (TextView) findViewById(R.id.textViewProfileName);
 		textViewProfileGender = (TextView) findViewById(R.id.textViewProfileGender);
 		textViewProfileFacebookID = (TextView) findViewById(R.id.textViewProfileFacebookID);
 		textViewProfileEmail = (TextView) findViewById(R.id.textViewProfileEmail);
-		textViewProfileName.setText(andrewID);
+
+		name = getIntent().getExtras().getString("name");
+		andrewID = getIntent().getExtras().getString("andrewID");
+		facebookID = getIntent().getExtras().getString("facebookID");
+		latitude = getIntent().getExtras().getDouble("userLatitude");
+		longitude = getIntent().getExtras().getDouble("userLongitude");
+
+		textViewProfileName.setText(name);
 		textViewProfileGender.setText(gender);
 		textViewProfileFacebookID.setText(facebookID);
-		textViewProfileEmail.setText(email);
+		textViewProfileEmail.setText(andrewID+'@'+getString(R.string.andrew_domain));
 		
 		buttonVisitFacebook = (Button) findViewById(R.id.buttonVisitFacebook);
-		buttonVisitFacebook.setOnClickListener(this);
-		
 		buttonSendFacebookMessage = (Button) findViewById(R.id.buttonSendFacebookMessage);
-		buttonSendFacebookMessage.setOnClickListener(this);
 		
 		buttonShowOnMap = (Button) findViewById(R.id.buttonShowOnMap);
 		buttonShowOnMap.setOnClickListener(this);
+
+		if(facebookNumberID == "") new SocialTask().getFacebookProfile();
 	}
 
 	@Override
@@ -68,15 +85,60 @@ public class ProfileActivity extends Activity implements OnClickListener {
 	}
 
 	private void visitFacebook() {
-
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/"+facebookNumberID)));
 	}
 
 	private void sendFacebookMessage() {
-		
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("fb://messaging/"+facebookNumberID)));
 	}
 
 	private void showOnMap() {
 		
 	}
 
+	class SocialTask extends AsyncTask<String, Void, String>{
+
+	    @Override
+	    protected String doInBackground(String... uri) {
+	        HttpClient httpClient = new DefaultHttpClient();
+	        HttpResponse httpResponse;
+	        String jsonResult = null;
+	        try {
+	            httpResponse = httpClient.execute(new HttpGet(uri[0]));
+	            if(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+	                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+	                httpResponse.getEntity().writeTo(buffer);
+	                buffer.close();
+	                jsonResult = buffer.toString();
+	            }
+	        } catch (IOException e) {
+                Log.d(getString(R.string.title_activity_profile), e.getMessage());
+	        }
+	        return jsonResult;
+	    }
+
+	    @Override
+	    protected void onPostExecute(String json) {
+	        super.onPostExecute(json);
+	        parseJSON(json);
+	        textViewProfileGender.setText(gender);
+	        buttonSendFacebookMessage.setOnClickListener(ProfileActivity.this);
+			buttonVisitFacebook.setOnClickListener(ProfileActivity.this);
+	    }
+
+	    private void parseJSON(String jsonResult) {
+            try {
+				JSONObject user = new JSONObject(jsonResult);
+				facebookNumberID = user.getString("id");
+				gender = user.getString("gender");
+			} catch (JSONException e) {
+				Log.d(getString(R.string.title_activity_profile),e.getMessage());
+			}
+	        Log.d(getString(R.string.title_activity_profile), facebookNumberID);
+	    }
+
+		private void getFacebookProfile() {
+			execute("http://graph.facebook.com/"+facebookID+"?fields=id,gender");
+		}
+	}
 }
